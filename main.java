743fdf54a70df3eb1095fde417bd48f8c8edf4c0
Nomo <100486782+GoIah@users.noreply.github.com>
2024-10-31 11:06:04 -0500

    added

--- main.java
+++ main.java
@@ -1,5 +1,23 @@
-public main {
-    public static void Main(string[] args) {
-        System.out.println("#ifyouknowyouknow");
+import java.util.Scanner;
+
+public class Main {
+    // made by an idiot
+    static int[] MemoryArray = new int[] {0,0,0,0, 0,0,0,0};
+    static int PI = 0; // Pointers Index
+    
+    public static void SRToutput() {
+      System.out.print("  ");
+      for (int i=0; i<MemoryArray.length; i++) {
+        System.out.print("["+MemoryArray[i]+"]");
+      }
+      System.out.print("\n"+("   ").repeat((PI)+1)+"^");
     }
+      
+    public static void main(String[] args) {
+      SRToutput();
+      
+    	Scanner input = new Scanner(System.in);
+      System.out.print("\n   Input: ");
+    	input.next();
+  }
 }
